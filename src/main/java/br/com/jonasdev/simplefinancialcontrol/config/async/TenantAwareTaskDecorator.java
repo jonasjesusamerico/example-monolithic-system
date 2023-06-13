b331eb12f92adad321c1226690b19105e88b4050
Jonas de Jesus Américo <jjsamerico@gmail.com>
2023-06-13 05:34:25 -0300

    init

---
+++ src/main/java/br/com/jonasdev/simplefinancialcontrol/config/async/TenantAwareTaskDecorator.java
@@ -0,0 +1,22 @@
+package br.com.jonasdev.simplefinancialcontrol.config.async;
+
+import br.com.jonasdev.simplefinancialcontrol.config.multitenancy.TenantContext;
+import org.springframework.core.task.TaskDecorator;
+import org.springframework.lang.NonNull;
+
+public class TenantAwareTaskDecorator implements TaskDecorator {
+
+    @Override
+    @NonNull
+    public Runnable decorate(@NonNull Runnable runnable) {
+        String tenantId = TenantContext.getTenantId();
+        return () -> {
+            try {
+                TenantContext.setTenantId(tenantId);
+                runnable.run();
+            } finally {
+                TenantContext.setTenantId(null);
+            }
+        };
+    }
+}
