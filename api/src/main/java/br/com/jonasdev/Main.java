eb04fb06d6b7080030bc7a974942d365ad785300
Jonas de Jesus Américo <jjsamerico@gmail.com>
2023-06-19 16:19:56 -0300

    ...

--- api/src/main/java/br/com/jonasdev/Main.java
+++ api/src/main/java/br/com/jonasdev/Main.java
@@ -3,8 +3,10 @@ package br.com.jonasdev;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class Main extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
